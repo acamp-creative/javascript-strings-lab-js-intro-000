3b344ec7ba62c173f8e50e57137f41581ea552a7
Anaella Campana <anaellacampanaa@gmail.com>
2020-07-02 21:00:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = var greeting + "Neil deGrasse Tyson"
+var specialGuest = greeting + "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
